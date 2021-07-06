57303197f1ae953d986cb55f209be67cfc3e8870
Agmar Torres <agmartorres@gmail.com>
2021-07-06 02:34:12 -0300

    feat: create domain and first request to api to get all movies

---
+++ src/domain/movies/index.ts
@@ -0,0 +1,20 @@
+interface SimpleMovie {
+	title: string;
+	episode_id: 4;
+	opening_crawl: string;
+	director: string;
+	producer: string;
+	release_date: string;
+	created: string;
+	edited: string;
+	url: string;
+}
+
+export interface MovieProps {
+	count: number;
+	results: Array<SimpleMovie>;
+}
+
+export const emptyMovie = (): MovieProps => {
+	return { count: 0, results: [] };
+};
--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,8 +1,21 @@
 import { Container, Header, Form, Grid } from './styles';
 import Input from '../../components/input';
 import MovieItem from '../../components/movie-item';
+import { emptyMovie, MovieProps } from './../../domain/movies/index';
+import api from '../../services/api';
+import React from 'react';
 
 const Movies = () => {
+	const [movies, setMovies] = React.useState<MovieProps>(emptyMovie);
+
+	React.useEffect(() => {
+		api
+			.get<MovieProps>('/films')
+			.then(({ data }) => {
+				setMovies(data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 	return (
 		<Container>
 			<Header>
