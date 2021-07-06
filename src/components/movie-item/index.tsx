216721ded997cfb44a72248c0e022d092e8a36b5
Agmar Torres <agmartorres@gmail.com>
2021-07-06 02:00:09 -0300

    feat: create MovieItem component

---
+++ src/components/movie-item/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Button } from './styles';
+
+const MoveItem = () => {
+	return (
+		<Container>
+			<h3> A New Hope</h3>
+			<span>Epsódio 4</span>
+			<h4>Lançado:</h4>
+			<p>30/04/2021</p>
+			<Button type="button">Selecionar</Button>
+		</Container>
+	);
+};
+
+export default MoveItem;
---
+++ src/components/movie-item/styles.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 250px;
+	height: 300px;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	background-color: ${({ theme }) => theme.colors.white};
+	border: 1px solid ${({ theme }) => theme.colors.secondary};
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+	border-radius: 20px;
+	transition: 1s ease;
+
+	h3 {
+		margin-top: 20px;
+		font-weight: bold;
+		font-size: ${({ theme }) => theme.sizes.fontSizes.normal};
+	}
+
+	span {
+		margin: 10px 0;
+		font-size: ${({ theme }) => theme.sizes.fontSizes.normal};
+		color: ${({ theme }) => theme.colors.gray};
+	}
+
+	h4 {
+		margin-top: 100px;
+		font-weight: bold;
+		font-size: ${({ theme }) => theme.sizes.fontSizes.normal};
+		color: ${({ theme }) => theme.colors.primary};
+	}
+
+	p {
+		color: ${({ theme }) => theme.colors.primary};
+	}
+
+	&:hover {
+		cursor: pointer;
+		transform: scale(1.1);
+		border: 3px solid inherit;
+	}
+`;
+
+export const Button = styled.button`
+	margin-top: 20px;
+	width: 80%;
+	height: 40px;
+	background-color: ${({ theme }) => theme.colors.white};
+	color: ${({ theme }) => theme.colors.primary};
+	font-size: ${({ theme }) => theme.sizes.fontSizes.normal};
+	border: 1px solid ${({ theme }) => theme.colors.primary};
+
+	border-radius: 5px;
+	&:hover {
+		background-color: ${({ theme }) => theme.colors.primary};
+		color: ${({ theme }) => theme.colors.white};
+	}
+`;
