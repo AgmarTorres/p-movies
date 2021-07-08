918328c46568fb1c9b88f7e8e4837a9e3c1df16d
Agmar Torres <agmartorres@gmail.com>
2021-07-08 09:25:39 -0300

    test: ensure input render and be used and changed value to filter films

--- src/components/input/input.spec.tsx
+++ src/components/input/input.spec.tsx
@@ -1,12 +1,16 @@
 import Input from '.';
 import { render, RenderResult, fireEvent } from '@testing-library/react';
+import SearchContext from '../../context/search';
 import { theme } from '../../styles/theme';
 import { ThemeProvider } from 'styled-components';
 
 const makeSut = (): RenderResult => {
+	const setSearch = jest.fn();
 	const sut = render(
 		<ThemeProvider theme={theme}>
-			<Input />
+			<SearchContext.Provider value={{ setSearch }}>
+				<Input />
+			</SearchContext.Provider>
 		</ThemeProvider>
 	);
 	return sut;
@@ -17,5 +21,13 @@ describe('SearchInput', () => {
 		const sut = makeSut();
 		const searchInput = sut.getByTestId('input');
 		fireEvent.input(searchInput, { target: { value: '' } });
+	});
+
+	it('should return the same value', () => {
+		const sut = makeSut();
+		const faker = 'value';
+		const searchInput = sut.getByTestId('input') as HTMLInputElement;
+		fireEvent.change(searchInput, { target: { value: faker } });
+		expect(searchInput.value).toBe(faker);
 	});
 });
